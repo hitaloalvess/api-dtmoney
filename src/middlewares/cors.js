97cbf1e5a73c5d0f93beb45b19729dd46bf2516f
hitaloalvess <hitalo.ralves@outlook.com>
2022-01-09 20:49:20 -0300

    Corrigindo acess-control-allow-origin cors

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,5 +1,5 @@
 const cors = (request, response, next) => {
-    response.setHeader("Access-Control-Allow-Origin", "https://hitaloalvess-dtmoney.herokuapp.com/");
+    response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     response.setHeader("Access-Control-Allow-Methods", "*");
     response.setHeader("Access-Control-Allow-Headers", "x-app-id");
     response.setHeader("Access-Control-Max-Age", "10");

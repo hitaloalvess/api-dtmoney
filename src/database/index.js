570d2e6448c681006a7206922013319b02ad548d
hitaloalvess <hitalo.ralves@outlook.com>
2022-01-09 16:20:35 -0300

    Corrigindo conexao com mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@dtmoney.ex4kk.mongodb.net/dtMoney?retryWrites=true&w=majority`);
+mongoose.connect(`mongodb+srv://hitalo1996:K9Czes7oVVFAoRKO@dtmoney.ex4kk.mongodb.net/dtMoney?retryWrites=true&w=majority`);
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
